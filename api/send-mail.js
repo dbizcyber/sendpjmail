8a57a50b54c9dd3e4b2b0e357acbcbf2c382cfc8
dbizcyber <dbizard@gmail.com>
2026-01-05 07:18:33 +0100

    Update send-mail.js
    modif avec api resend

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,7 +1,8 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
-  // Autoriser uniquement POST
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -13,42 +14,27 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "No image provided" });
     }
 
-    // Nettoyage base64 (PNG uniquement)
     const base64Data = imageBase64.replace(
       /^data:image\/png;base64,/,
       ""
     );
 
-    const imageBuffer = Buffer.from(base64Data, "base64");
-
-    // Transport SMTP (Gmail exemple)
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-      }
-    });
-
-    // Envoi du mail
-    await transporter.sendMail({
-      from: `"Web La Marmotte" <${process.env.MAIL_USER}>`,
-      to: "contact@lamarmottechateaurenard.com",
-      cc: "lamarmotterando@gmail.com",
+    await resend.emails.send({
+      from: "Rando <onboarding@resend.dev>", // temporaire
+      to: ["contact@tonsite.fr"],            // DESTINATAIRE
       subject: subject || "Profil altimétrique",
       text: message || "Voir pièce jointe",
       attachments: [
         {
           filename: "profil-altimetrique.png",
-          content: imageBuffer,
-          contentType: "image/png"
+          content: base64Data
         }
       ]
     });
 
     return res.status(200).json({ success: true });
   } catch (error) {
-    console.error("MAIL ERROR:", error);
+    console.error(error);
     return res.status(500).json({ error: "Mail sending failed" });
   }
 }
